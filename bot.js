6004cbb368f65dc0b730eff1070a16652550f9db
ALisaklolas <35742714+ALisaklolas@users.noreply.github.com>
2018-10-08 23:05:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,39 +9,14 @@ client.on('ready', () => {
 });
 
 
-const YTDL = require('ytdl-core');
-const nodeopus = require('node-opus');
-const ffmpeg = require('ffmpeg');
-var servers = {};
-function play(connection, message, args) {
-  var server = servers[message.guild.id];
-  server.dispatcher = connection.playStream(YTDL(args[0]), {filter: "audioonly"});
-  server.queue.shift();
-  server.dispatcher.on("end", function() {
-    if (server.queue[0]) play(connection, message);
-    else connection.disconnect();
-  });
-}
-
-
-client.on('message', message =>{
-  if(message.content.startsWith('join')){
-    const voiceChannel = message.member.voiceChannel
-    voiceChannel.join();
-    message.channel.send("Connected to voice channel")
-}})
-
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+lient.on('ready', () => {
+    let channel_id = "485549337447890944";
+    var channel = client.channels.get(channel_id);
+    if(channel.type == 'voice') {
+        channel.join();
+    } else {
+        console.log('...');
+    }
 });
-
-client.on('message', message => {
- if(message.content.startsWith(prefix + "savagejoin")) {
-message.member.voiceChannel.join().catch(e => message.channel.send(e));
-}
-});
-
-
 
 client.login(process.env.BOT_TOKEN);
