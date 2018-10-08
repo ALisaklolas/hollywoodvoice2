0076089eb81dffb5ef274e0a3732409607802fb3
ALisaklolas <35742714+ALisaklolas@users.noreply.github.com>
2018-10-08 23:22:58 +0200

    Create bot2.js

---
+++ bot2.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '.'
+
+client.on('ready', () => {
+  console.log('---------------');
+  console.log(' Bot Is Online')
+  console.log('---------------')
+});
+
+
+client.on('ready', () => {
+    let channel_id = "485549337447890944";
+    var channel = client.channels.get(channel_id);
+    if(channel.type == 'voice') {
+        channel.join();
+    } else {
+        console.log('...');
+    }
+});
+
+client.login(process.env.BOT1_TOKEN);
